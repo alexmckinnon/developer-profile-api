acfe217c921703b59ae17ce87bb833783dd0f284
Alex McKinnon <alex.r.mckinnon@gmail.com>
2019-09-16 23:29:21 -0700

    Added body-parser package and POST endpoint to save new user

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const express = require('express')
-const app = express()
-const sqlite3 = require('sqlite3').verbose()
+const express = require('express');
+const app = express();
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+
+// Set up body-parser to accept json data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Start server and listen on port 80
-app.listen(80)
+app.listen(80);
 
 // Set up sqlite database
 let db = new sqlite3.Database('developer-profile.db', (err) => {
@@ -17,22 +22,18 @@ let db = new sqlite3.Database('developer-profile.db', (err) => {
         github text
     )`, (err) => {
         if (err) {
-            console.log(err.message)
+            console.log(err.message);
             return;
         }
-        // db.run(`INSERT INTO users (email, linkedin, github) 
-        //         VALUES ("admin@example.com","admin", "admin")`)
     });
 });
 
 /**
  * Retrieve user data
  * Returns json data containing user account names
- * @param {String} email
- * @returns {String}
  */
 app.get('/user/:email', (req, res) => {
-    let sql = "SELECT * FROM users WHERE email = ?"
+    let sql = "SELECT * FROM users WHERE email = ?";
     db.get(sql, [req.params.email], (err, row) => {
         // Error running query
         if (err) {
@@ -43,7 +44,7 @@ app.get('/user/:email', (req, res) => {
             return res.status(404).json({ "error": "No user found" });
         }
         // Return json data
-        res.json({ "data": row })
+        res.json({ "data": row });
     })
 })
 
@@ -51,5 +52,27 @@ app.get('/user/:email', (req, res) => {
  * Store user data
  */
 app.post('/user', (req, res) => {
-    // Save user data here
+    const data = req.body.data;
+    const {email, github, linkedin} = data;
+    if (!email || !github || !linkedin) {
+        return res.status(400).json({ "error": "Missing parameters" });
+    }
+    // Check for existing user
+    let sql = "SELECT * FROM users WHERE email = ?";
+    db.get(sql, [email], (err, row) => {
+        // Error running query
+        if (err) {
+            return res.status(400).json({ "error": err.message });
+        }
+        // User already exists
+        if (row) {
+            return res.status(409).json({ "error": "User already exists" });
+        } else {
+            // Save new user
+            let stmt = db.prepare('INSERT INTO users VALUES (?, ?, ?)');
+            stmt.run(email, github, linkedin);
+            stmt.finalize();
+            return res.status(201).end();
+        }
+    })
 })
